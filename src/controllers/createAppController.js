e1f0652dbd20cd1f1a3c7667a84a2ce5c0e989d7
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-03 09:41:07 +1000

    successfully call api for sydney and log data to understand the json structure

--- src/controllers/createAppController.js
+++ src/controllers/createAppController.js
@@ -3,6 +3,13 @@ const createAppController = () => {
 
   const getWeather = async (location) => {
     try {
+      const response = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`,
+        { mode: "cors" },
+      );
+      const data = await response.json();
+
+      console.log(data);
     } catch (error) {
       console.error(
         "There has been a problem with fetching from the WeatherAPI",
