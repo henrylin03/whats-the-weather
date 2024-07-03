ee95c87ae46e4ba0c15b741a0ac15dc3015fe956
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-03 12:03:56 +1000

    update forecast cards' conditions from the json object

--- src/components/forecast/displayForecast.js
+++ src/components/forecast/displayForecast.js
@@ -1,7 +1,11 @@
 const forecastSection = document.querySelector("#forecast");
-const cardsArray = [...forecastSection.children];
+const cards = [...forecastSection.children];
 
 const displayForecast = (forecastArr) => {
+  forecastArr.forEach((day, idx) => {
+    const weatherText = cards[idx].querySelector(".weather");
+    weatherText.textContent = day.day.condition.text;
+  });
   return;
 };
 
