361352efd77c942ef75c3def2eb83ac0d84cc7b4
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-03 14:28:50 +1000

    remove comments

--- src/components/forecast/index.js
+++ src/components/forecast/index.js
@@ -9,21 +9,10 @@ const forecast = async (location) => {
     const weatherData = await app.getWeather(location);
     const forecastArray = weatherData.forecast.forecastday;
 
-    console.log(forecastArray);
     displayForecast(forecastArray);
   } catch (error) {
     console.error("Error when getting forecast data:", error);
   }
 };
-
-// find day of the week for the last day (the first two are just today and tomorrow)
-
-// for each of the days in .forecast.forecastday...
-
-// generate the div
-
-// get the actual .day.condition.text (and then add the appropriate icon - they give you one in api but i like my tabler ones more!)
-
-// get the maxtemp_c, and mintemp_c
 
 export default forecast;
