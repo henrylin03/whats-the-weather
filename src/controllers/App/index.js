d3e291126060f039cb4a1bf6d9f69a89b112f092
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-03 11:36:34 +1000

    make variable more descriptive

--- src/controllers/App/index.js
+++ src/controllers/App/index.js
@@ -2,8 +2,8 @@ import getWeather from "./getWeather";
 
 const createAppController = () => {
   const getForecastData = async (location) => {
-    const data = await getWeather(location);
-    const forecastArray = await data.forecast.forecastday;
+    const weatherData = await getWeather(location);
+    const forecastArray = await weatherData.forecast.forecastday;
 
     console.log(forecastArray);
     return forecastArray;
