65cafdd5c1bbd3c00f4bc5cc0783a2fee5a7ce4e
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-03 15:05:30 +1000

    select elements from DOM

--- src/components/todayDetails/displayDetails.js
+++ src/components/todayDetails/displayDetails.js
@@ -1,3 +1,17 @@
+const weatherIconElement = document.querySelector(
+  "#today-details .weather .icon",
+);
+const currentTemperatureElement = document.querySelector(
+  "#today-details .temperature-text",
+);
+const chanceOfRainElement = document.querySelector(
+  "#today-details .chance-of-rain .property-value",
+);
+const windSpeedElement = document.querySelector(
+  "#today-details .wind-speed .property-value",
+);
+const updatedTimeElement = document.querySelector(".time");
+
 const displayDetails = () => {
   return;
 };
