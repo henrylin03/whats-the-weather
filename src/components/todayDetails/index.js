7aedabac743884437c9a5099bf2810cf2b6dbf74
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-03 14:58:03 +1000

    turn todayDetails component into an async function
    this is so that it actually awaits for the weather data from the api to come first

--- src/components/todayDetails/index.js
+++ src/components/todayDetails/index.js
@@ -1,8 +1,17 @@
 import "./index.css";
 import createAppController from "../../controllers/App";
 
-const todayDetails = () => {
-  return;
+const todayDetails = async (location) => {
+  const app = createAppController();
+
+  try {
+    const weatherData = await app.getWeather(location);
+    const currentWeatherData = weatherData.current;
+
+    console.log(currentWeatherData);
+  } catch (error) {
+    console.error("Error when getting forecast data:", error);
+  }
 };
 
 export default todayDetails;
