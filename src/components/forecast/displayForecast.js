294830bf53cd001cad1f14a32f0eddc892b181f2
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-03 12:09:59 +1000

    add spans in html element for temperature to make it easier to select from DOM in js

--- src/components/forecast/displayForecast.js
+++ src/components/forecast/displayForecast.js
@@ -5,6 +5,8 @@ const displayForecast = (forecastArr) => {
   forecastArr.forEach((day, idx) => {
     const weatherText = cards[idx].querySelector(".weather");
     weatherText.textContent = day.day.condition.text;
+
+    // todo: icon for weather condition (start with using the api's i think)
   });
   return;
 };
