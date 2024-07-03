287da62558664ade73acd8a23e407e0db4a8e11d
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-03 12:24:57 +1000

    display the day of the week using a utility function .getDayOfTheWeek

--- src/components/forecast/displayForecast.js
+++ src/components/forecast/displayForecast.js
@@ -5,6 +5,12 @@ const displayForecast = (forecastArr) => {
   forecastArr.forEach((day, idx) => {
     const currentCard = cards[idx];
 
+    // any days after 'today' and 'tomorrow
+    if (idx > 1) {
+      const dayElement = currentCard.querySelector(".day");
+      dayElement.textContent = getDayOfTheWeek(day.date);
+    }
+
     const weatherTextElement = currentCard.querySelector(".weather");
     weatherTextElement.textContent = day.day.condition.text;
 
@@ -23,6 +29,20 @@ const displayForecast = (forecastArr) => {
     const chanceOfRain = day.day.daily_chance_of_rain;
     chanceOfRainElement.textContent = `${chanceOfRain}%`;
   });
+};
+
+const getDayOfTheWeek = (dateStr) => {
+  const DAY_NAMES = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dateObject = new Date(dateStr);
+  return DAY_NAMES[dateObject.getDay()];
 };
 
 export default displayForecast;
