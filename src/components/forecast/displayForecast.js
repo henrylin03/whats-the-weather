5e855bd35e1f3064e77fa5056af3a7a5fd151382
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-03 12:14:30 +1000

    display max and minimum temperatures in forecast

--- src/components/forecast/displayForecast.js
+++ src/components/forecast/displayForecast.js
@@ -3,12 +3,20 @@ const cards = [...forecastSection.children];
 
 const displayForecast = (forecastArr) => {
   forecastArr.forEach((day, idx) => {
-    const weatherText = cards[idx].querySelector(".weather");
+    const currentCard = cards[idx];
+
+    const weatherText = currentCard.querySelector(".weather");
     weatherText.textContent = day.day.condition.text;
 
     // todo: icon for weather condition (start with using the api's i think)
+
+    const maxTempText = currentCard.querySelector(".max-temp");
+    const maxTemp = day.day.maxtemp_c.toFixed(0);
+    maxTempText.textContent = `${maxTemp}°`;
+    const minTempText = currentCard.querySelector(".min-temp");
+    const minTemp = day.day.mintemp_c.toFixed(0);
+    minTempText.textContent = `/${minTemp}°`;
   });
-  return;
 };
 
 export default displayForecast;
