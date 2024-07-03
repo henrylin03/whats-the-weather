3f630934c77eef3258a196bd9acadcf0a8338617
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-07-03 10:36:35 +1000

    write function to test call the Weather API to get current and forecast weather data (#19)
    * only call appController factory from within screenController in preparation for appController's method to get the weather at a particular location. used sydney as a placeholder value to test
    
    * add api key
    
    * simplify method name. add async keyword. use try/catch blocks to catch errors when attempting to retrieve weather data
    
    * successfully call api for sydney and log data to understand the json structure
    
    * use the forecast api instead of current because the forecast also encompasses current weather. set forecast days to the maximum (3 days)
    
    * import ui components into screen controller instead
    
    this makes more sense because the screen controller is the 'orchestrator'

--- src/controllers/createScreenController.js
+++ src/controllers/createScreenController.js
@@ -1,5 +1,14 @@
+import createAppController from "./createAppController";
+import header from "../components/header";
+import todayDetails from "../components/todayDetails";
+import forecast from "../components/forecast";
+
 const createScreenController = () => {
-  console.log("screen controller added");
+  const app = createAppController();
+  const locationName = "Sydney";
+
+  app.getWeather(locationName);
+
   return;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import "./assets/styles/normalise.css";
 import "./assets/styles/global.css";
-import header from "./components/header";
-import todayDetails from "./components/todayDetails";
-import forecast from "./components/forecast";
 import createScreenController from "./controllers/createScreenController";
-import createAppController from "./controllers/createAppController";
 
 createScreenController();
-createAppController();
