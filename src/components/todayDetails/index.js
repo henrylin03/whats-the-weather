968ea40126967e693c6aec6dfacdabef74ef3368
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-03 14:44:04 +1000

    import todayDetails' index.js into the screen controller factory

--- src/components/todayDetails/index.js
+++ src/components/todayDetails/index.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import createAppController from "../../controllers/App";
 
 const todayDetails = () => {
   return;
--- src/controllers/Screen/index.js
+++ src/controllers/Screen/index.js
@@ -6,6 +6,7 @@ const createScreenController = () => {
   const locationName = "Sydney";
 
   // run
+  todayDetails(locationName);
   forecast(locationName);
 };
 
