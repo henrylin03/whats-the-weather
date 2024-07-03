4b71a951e54e138ecf89c85593b9c0c3debf48e1
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-03 14:38:26 +1000

    remove unused initialisation of app controller in screenController factory

--- src/controllers/Screen/index.js
+++ src/controllers/Screen/index.js
@@ -1,10 +1,8 @@
-import createAppController from "../App";
 import header from "../../components/header";
 import todayDetails from "../../components/todayDetails";
 import forecast from "../../components/forecast";
 
 const createScreenController = () => {
-  const app = createAppController();
   const locationName = "Sydney";
 
   // run
