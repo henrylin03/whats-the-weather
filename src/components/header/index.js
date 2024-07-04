f3b3164c6c086ae13d8164f24d2793079f261ce8
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-04 15:28:27 +1000

    make event listener for the search bar be that, on input of any text, we search for the city, and print the results in the console for now

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,8 @@
 import "./index.css";
-import { searchCity } from "../../controllers/ApiController";
 
 const header = (searchStr) => {
   // initial state of the app will be sydney
   // on input of any non-space text in the search for city (event)...
-  // make api call to the "search/autocomplete" api for location [need branch to test this in console to see what actually comes back]
-  searchCity(searchStr);
 };
 
 export default header;
--- src/controllers/ScreenController.js
+++ src/controllers/ScreenController.js
@@ -1,16 +1,16 @@
 import header from "../components/header";
 import todayDetails from "../components/todayDetails";
 import forecast from "../components/forecast";
+import { searchCity } from "./ApiController";
 
 const createScreenController = () => {
   const searchBarElement = document.querySelector("#searchbar");
-
   const locationName = "sdy";
 
   // api seems to only kick in after at least 3 characters are put in... don't call api until then
 
   searchBarElement.addEventListener("input", () =>
-    console.log(searchBarElement.value),
+    searchCity(searchBarElement.value),
   );
 
   // run
