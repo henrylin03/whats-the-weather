32598f4598cb26ed56028c18064ada041dff1f7c
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-04 15:42:57 +1000

    set initial city to be sydney, and show that in the header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,8 @@
 import "./index.css";
 
-const header = (searchStr) => {
-  // initial state of the app will be sydney
-  // on input of any non-space text in the search for city (event)...
+const header = (location) => {
+  const searchBarElement = document.querySelector("#searchbar");
+  searchBarElement.value = location;
 };
 
 export default header;
--- src/controllers/ScreenController.js
+++ src/controllers/ScreenController.js
@@ -5,11 +5,10 @@ import { searchCity } from "./ApiController";
 
 const createScreenController = () => {
   const searchBarElement = document.querySelector("#searchbar");
-  const locationName = "Sydney, Australia";
+  let locationName = "Sydney, Australia";
 
   searchBarElement.addEventListener("input", (e) => {
     const searchInput = searchBarElement.value;
-
     if (searchInput.trim().length < 3 || e.inputType == "deleteContentBackward")
       return;
     searchCity(searchBarElement.value);
