4d9a1516795dcfb17e3a8beae1502a8202b50703
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-04 15:55:51 +1000

    when a user clicks the text in the search bar, it is already fully selected, ready for the user to completely remove. fixes #27

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 const header = (location) => {
   const searchBarElement = document.querySelector("#searchbar");
   searchBarElement.value = location;
+  searchBarElement.addEventListener("focus", () => searchBarElement.select());
 };
 
 export default header;
