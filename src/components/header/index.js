e328d2201cad25c69932092cd6b78d79e0c3aff6
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-04 12:12:21 +1000

    add comments as pseudocode for now

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,9 @@ import "./index.css";
 const searchBarElement = document.querySelector("#searchbar");
 
 const header = () => {
-  return;
+  // initial state of the app will be sydney
+  // on input of any non-space text in the search for city (event)...
+  // make api call to the "search/autocomplete" api for location [need branch to test this in console to see what actually comes back]
 };
 
 export default header;
