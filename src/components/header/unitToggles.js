b0d0b6c2a1d64f4b3044770bf50ea1fc25264359
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-05 20:07:52 +1000

    update name of handler to be clearer
    because actual changing of units need to happen at the screenController orchestration level as changing the unit changes the whole screen technically

--- src/components/header/unitToggles.js
+++ src/components/header/unitToggles.js
@@ -2,14 +2,14 @@ const btnsContainer = document.querySelector(".unit-btns");
 const unitBtns = [...btnsContainer.children];
 
 const unitToggles = () => {
-  const handleClickOnUnitBtn = (e) => {
+  const updateSelectedUnit = (e) => {
     if (e.target.classList.contains("selected")) return;
     unitBtns.forEach((btn) => btn.classList.remove("selected"));
     e.target.classList.add("selected");
   };
 
   unitBtns.forEach((btn) =>
-    btn.addEventListener("mousedown", handleClickOnUnitBtn),
+    btn.addEventListener("mousedown", updateSelectedUnit),
   );
 };
 
