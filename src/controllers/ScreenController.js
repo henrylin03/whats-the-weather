09be20370ef296f65e83d3c7c8ce5b844486f7a5
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-05 16:30:22 +1000

    clear all children of the results unordered list element to start

--- src/controllers/ScreenController.js
+++ src/controllers/ScreenController.js
@@ -33,9 +33,12 @@ const handleSearchInput = async (e) => {
   if (searchInput.trim().length < 3) return;
 
   const resultsArray = await searchCity(searchBarElement.value);
+  console.log(resultsArray); // todo: remove
   const noResults = !resultsArray || !resultsArray.length;
 
-  if (noResults) searchResultsContainerElement.classList.add("no-results");
+  if (noResults)
+    return searchResultsContainerElement.classList.add("no-results");
+
   // if async/await returns non-empty array, display those results in the div
 
   //todo: clearing the text input OR calling again means the error message goes away
@@ -43,7 +46,17 @@ const handleSearchInput = async (e) => {
   // ? how do we then return the actual current location back to the text box? maybe on no focus?
 
   // nb: it's possible that we need to clear the ul every time
-  return;
+};
+
+const displaySearchResults = (resultsArray) => {
+  const resultsListElement = document.querySelector(".results-container ul");
+
+  // clears all previous items
+  resultsListElement.replaceChildren();
+
+  // for each of the elements, generate a list item [see html now for what to generate]
+
+  // don't worry about shortening names for now. but i think each of the buttons should have the 'id' of the location as a data-attribute so that it can be picked up when clicked...
 };
 
 export default createScreenController;
