404ead6b7b021e8f0d8785fd95d632fb8ee85a7e
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-05 16:37:43 +1000

    display the search results in the dropdown under the searchbar

--- src/controllers/ScreenController.js
+++ src/controllers/ScreenController.js
@@ -38,14 +38,13 @@ const handleSearchInput = async (e) => {
 
   if (noResults)
     return searchResultsContainerElement.classList.add("no-results");
-
-  // if async/await returns non-empty array, display those results in the div
+  searchResultsContainerElement.classList.remove("no-results");
+  searchResultsContainerElement.classList.add("results");
+  displaySearchResults(resultsArray);
 
   //todo: clearing the text input OR calling again means the error message goes away
 
   // ? how do we then return the actual current location back to the text box? maybe on no focus?
-
-  // nb: it's possible that we need to clear the ul every time
 };
 
 const displaySearchResults = (resultsArray) => {
@@ -65,7 +64,7 @@ const displaySearchResults = (resultsArray) => {
     resultsListElement.appendChild(item);
   });
 
-  // don't worry about shortening names for now. but i think each of the buttons should have the 'id' of the location as a data-attribute so that it can be picked up when clicked...
+  // don't worry about shortening names for now
 };
 
 export default createScreenController;
