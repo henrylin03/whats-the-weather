6c198d7f70f842abfe9c54f8b567d9ace93ec2ab
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-05 19:45:53 +1000

    clicking the buttons toggles which one is selected and updates the styling accordingly. if the currently selected unit is already clicked, we exit using guard clause to avoid unnecessary further work

--- src/components/header/unitToggles.js
+++ src/components/header/unitToggles.js
@@ -1,5 +1,16 @@
 const unitToggles = () => {
-  return;
+  const btnsContainer = document.querySelector(".unit-btns");
+  const unitBtns = [...btnsContainer.children];
+
+  const handleClickOnUnitBtn = (e) => {
+    if (e.target.classList.contains("selected")) return;
+    unitBtns.forEach((btn) => btn.classList.remove("selected"));
+    e.target.classList.add("selected");
+  };
+
+  unitBtns.forEach((btn) =>
+    btn.addEventListener("mousedown", handleClickOnUnitBtn),
+  );
 };
 
 export default unitToggles;
