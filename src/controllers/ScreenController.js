6c20bbc790b52ab501fe3f2b90c4d4fc9fa2537c
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-05 16:24:49 +1000

    show error message in dropdown if there is no result

--- src/controllers/ScreenController.js
+++ src/controllers/ScreenController.js
@@ -27,15 +27,20 @@ const createScreenController = () => {
 };
 
 const handleSearchInput = async (e) => {
+  const searchResultsContainerElement =
+    document.querySelector(".results-container");
   const searchInput = searchBarElement.value;
   if (searchInput.trim().length < 3) return;
 
   const resultsArray = await searchCity(searchBarElement.value);
   const noResults = !resultsArray || !resultsArray.length;
 
-  // if async/await returns an empty array, then display the no-results div
-
+  if (noResults) searchResultsContainerElement.classList.add("no-results");
   // if async/await returns non-empty array, display those results in the div
+
+  //todo: clearing the text input OR calling again means the error message goes away
+
+  // ? how do we then return the actual current location back to the text box? maybe on no focus?
 
   // nb: it's possible that we need to clear the ul every time
   return;
