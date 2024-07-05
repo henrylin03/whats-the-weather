62a0523ad0db6c6afbbd5880d695cdce27a44ffe
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-05 17:20:23 +1000

    move most of the logic relating to the search bar to the header component's index.js
    this makes more sense. the screenController should just be importing most things and orchestrating the entire screen. the display() function certainly does this

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,55 @@
 import "./index.css";
+import { searchCity } from "../../controllers/ApiController";
+
+const searchBarElement = document.querySelector("#searchbar");
+const searchResultsContainerElement =
+  document.querySelector(".results-container");
 
 const header = (location) => {
-  const searchBarElement = document.querySelector("#searchbar");
   searchBarElement.value = location;
+
+  searchBarElement.addEventListener("input", handleSearchInput);
+  searchBarElement.addEventListener("focus", handleSearchInput);
+  searchBarElement.addEventListener("blur", hideSearchResultsContainer);
   searchBarElement.addEventListener("focus", () => searchBarElement.select());
+};
+
+const handleSearchInput = async (e) => {
+  hideSearchResultsContainer();
+
+  const searchInput = searchBarElement.value;
+  if (searchInput.trim().length < 3) return;
+
+  const resultsArray = await searchCity(searchBarElement.value);
+  const noResults = !resultsArray || !resultsArray.length;
+
+  if (noResults)
+    return searchResultsContainerElement.classList.add("no-results");
+  searchResultsContainerElement.classList.add("results");
+  displaySearchResults(resultsArray);
+};
+
+const displaySearchResults = (resultsArray) => {
+  const resultsListElement = document.querySelector(".results-container ul");
+
+  // clears all previous items
+  resultsListElement.replaceChildren();
+
+  resultsArray.forEach((location) => {
+    const item = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.type = "button";
+    btn.textContent = `${location.name}, ${location.region}, ${location.country}`;
+    btn.setAttribute("data-location-id", location.id);
+
+    item.appendChild(btn);
+    resultsListElement.appendChild(item);
+  });
+};
+
+const hideSearchResultsContainer = () => {
+  searchResultsContainerElement.classList.remove("no-results");
+  searchResultsContainerElement.classList.remove("results");
 };
 
 export default header;
--- src/controllers/ScreenController.js
+++ src/controllers/ScreenController.js
@@ -1,13 +1,10 @@
 import header from "../components/header";
 import todayDetails from "../components/todayDetails";
 import forecast from "../components/forecast";
-import { searchCity } from "./ApiController";
-
-const searchBarElement = document.querySelector("#searchbar");
-const searchResultsContainerElement =
-  document.querySelector(".results-container");
 
 const createScreenController = () => {
+  const searchBarElement = document.querySelector("#searchbar");
+
   let locationName = "Sydney, New South Wales, Australia";
 
   const display = (location = locationName) => {
@@ -16,11 +13,6 @@ const createScreenController = () => {
     forecast(location);
   };
 
-  searchBarElement.addEventListener("input", handleSearchInput);
-  searchBarElement.addEventListener("focus", handleSearchInput);
-  searchBarElement.addEventListener("blur", hideSearchResultsContainer);
-
-  //
   searchBarElement.addEventListener("keyup", ({ key }) => {
     if (key === "Enter") {
       locationName = searchBarElement.value;
@@ -30,44 +22,6 @@ const createScreenController = () => {
 
   // run
   display();
-};
-
-const hideSearchResultsContainer = () => {
-  searchResultsContainerElement.classList.remove("no-results");
-  searchResultsContainerElement.classList.remove("results");
-};
-
-const handleSearchInput = async (e) => {
-  hideSearchResultsContainer();
-
-  const searchInput = searchBarElement.value;
-  if (searchInput.trim().length < 3) return;
-
-  const resultsArray = await searchCity(searchBarElement.value);
-  const noResults = !resultsArray || !resultsArray.length;
-
-  if (noResults)
-    return searchResultsContainerElement.classList.add("no-results");
-  searchResultsContainerElement.classList.add("results");
-  displaySearchResults(resultsArray);
-};
-
-const displaySearchResults = (resultsArray) => {
-  const resultsListElement = document.querySelector(".results-container ul");
-
-  // clears all previous items
-  resultsListElement.replaceChildren();
-
-  resultsArray.forEach((location) => {
-    const item = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.type = "button";
-    btn.textContent = `${location.name}, ${location.region}, ${location.country}`;
-    btn.setAttribute("data-location-id", location.id);
-
-    item.appendChild(btn);
-    resultsListElement.appendChild(item);
-  });
 };
 
 export default createScreenController;
