327a2c8924846de3712315e234764955adb264bb
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-05 20:30:40 +1000

    initial push to github pages

--- README.md
+++
@@ -1,3 +0,0 @@
-# whats-the-weather
-
-Web app to tell you the weather in a particular location. Written in HTML/CSS and vanilla JavaScript, bundled with Webpack 5.
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import "./assets/styles/normalise.css";
-import "./assets/styles/global.css";
-import createScreenController from "./controllers/ScreenController";
-
-createScreenController();
