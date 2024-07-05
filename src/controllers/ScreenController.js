1caa36a0ef15ea0812dd3b3b8370b87ba5d9b303
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-05 16:14:36 +1000

    have event handler for handling inputting into search bar for easier maintainability

--- src/controllers/ScreenController.js
+++ src/controllers/ScreenController.js
@@ -14,13 +14,7 @@ const createScreenController = () => {
     forecast(location);
   };
 
-  searchBarElement.addEventListener("input", (e) => {
-    const searchInput = searchBarElement.value;
-    if (searchInput.trim().length < 3 || e.inputType == "deleteContentBackward")
-      return;
-    searchCity(searchBarElement.value);
-  });
-
+  searchBarElement.addEventListener("input", handleSearchInput);
   searchBarElement.addEventListener("keyup", ({ key }) => {
     if (key === "Enter") {
       locationName = searchBarElement.value;
@@ -32,7 +26,20 @@ const createScreenController = () => {
   display();
 };
 
-const showResults = () => {
+const handleSearchInput = (e) => {
+  const searchInput = searchBarElement.value;
+  if (searchInput.trim().length < 3 || e.inputType == "deleteContentBackward")
+    return;
+
+  // use async/await to await for the array that gets returned by searchCity...
+
+  // if async/await returns an empty array, then display the no-results div
+
+  // if async/await returns non-empty array, display those results in the div
+
+  // nb: it's possible that we need to clear the ul every time
+
+  searchCity(searchBarElement.value);
   return;
 };
 
